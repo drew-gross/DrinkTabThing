e60e39eb06fa52f8972d0bc6e8aceb23f4deeb1a
Drew Gross <drew.a.gross@gmail.com>
2013-08-10 20:28:54 -0700

    bars page. Can create new tab

--- DrinkTabThing.js
+++ DrinkTabThing.js
@@ -1,10 +1,16 @@
 Tabs = new Meteor.Collection('tabs');
 Drinks = new Meteor.Collection('drinks');
 PurcahsedDrinks = new Meteor.Collection('purcahsed_drinks');
+Bars = new Meteor.Collection('bars');
 
 if (Meteor.isClient) {
   Meteor.Router.add({
     '/': 'home',
+
+    '/bar/:id': function (id) {
+      Session.set('barId', id);
+      return 'bar';
+    },
 
     '/tabs/:id':  function (id) {
       Session.set('tabId', id);
@@ -20,6 +26,18 @@ if (Meteor.isClient) {
     return PurcahsedDrinks.find({tab:Session.get('tabId')});
   };
 
+  Template.bar.bar_name = function(){
+    var bar = Bars.findOne({_id:Session.get('barId')});
+    return bar && bar.name;
+  };
+
+  Template.bar.events({
+    'click .open_tab': function () {
+      var new_tab_id = Tabs.insert({owner:"Drew Gross"});
+      Meteor.Router.to('/tabs/' + new_tab_id);
+    }
+  });
+
   Template.drink_from_menu.events({
     'click .buy_drink': function () {
       PurcahsedDrinks.insert({name:this.name, price:this.price, tab:Session.get('tabId')});
@@ -29,10 +47,5 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    Meteor.Router.add({
-      '/tabs/:id.xml': function(id) {
-        return Tabs.findOne(id).owner;
-      }
-    });
   });
 }
