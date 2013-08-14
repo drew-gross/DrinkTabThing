078e825897384237248777277debd2de047f6cc0
Guilherme Decampo <guilherme.decampo@gmail.com>
2013-08-14 01:09:44 -0700

    error public js

--- js/drinkzy.js
+++ js/drinkzy.js
@@ -70,34 +70,6 @@ $(document).ready(function() {
 	});
 
 	// feature detection and polyfill	
-	var theForm = $('.ie8 form');
-	
-	theForm.find('input, textarea').each(function(){
-		
-		// if HTML5 input placeholder is not supported
-		var placeholderText = $(this).attr('placeholder');
-		
-		if(placeholderText){
-			
-			$(this)
-				.addClass('input-placeholder')
-				.val(placeholderText)
-				.bind('focus', function(){
-				
-					if($(this).val() == placeholderText ){
-						
-						$(this).val('').removeClass('input-placeholder');
-					}
-				
-				})
-				.bind('blur', function(){
-					
-					if($(this).val() == '' ){
-					
-						$(this).val(placeholderText).addClass('input-placeholder');
-					}
-				});
-		}				
-	});
+
 
 });
